59931c6b8716223b148cf9fda5bef87cb17ad955
Dave Mackey <dave@davemackey.net>
2021-01-09 20:57:18 -0500

    Refactor fetchRemoteDict... to fetchMarkData

--- markasread/background.js
+++ markasread/background.js
@@ -5,8 +5,8 @@
  * Reference: https://developer.chrome.com/docs/extensions/reference/runtime/
  */
 chrome.runtime.onInstalled.addListener(function () {
-	// console.log("onInstalled");
-	fetchRemoteDictionary();
+	console.log("onInstalled");
+	fetchMarkData();
 })
 
 /**
@@ -16,9 +16,9 @@ chrome.runtime.onInstalled.addListener(function () {
  * Reference: https://developer.chrome.com/docs/extensions/reference/runtime/
  */
 chrome.runtime.onStartup.addListener(function () {
-	// console.log("onStartup");
+	console.log("onStartup");
 	visited = {};
-	fetchRemoteDictionary();
+	fetchMarkData();
 });
 
 /**
@@ -29,7 +29,7 @@ chrome.browserAction.onClicked.addListener(function(tabs) {
 	// Return if the tab is active and in current window.
 	// Reference: https://developer.chrome.com/docs/extensions/reference/tabs/
 	chrome.tabs.query({'active': true, 'currentWindow': true}, function (tab) {
-		// console.log(tab[0].url);
+		console.log(tab[0].url);
 		if (!markedAsRead(tab[0].url)) {
 			addUrl(tab[0].url);
 			markAsVisited(tab[0].id);
@@ -44,7 +44,7 @@ chrome.browserAction.onClicked.addListener(function(tabs) {
 * On tab activation, change mark as read icon based on status.
 */
 chrome.tabs.onActivated.addListener(function callback(activeInfo) {
-	// console.log("onActivated");
+	console.log("onActivated");
 	chrome.tabs.query({'active': true, 'currentWindow': true}, function (tab) {
 		console.log(tab[0].url);
 		if (!markedAsRead(tab[0].url)) {
@@ -61,7 +61,7 @@ chrome.tabs.onActivated.addListener(function callback(activeInfo) {
  * Reference: https://developer.chrome.com/docs/extensions/reference/tabs/
  */
 chrome.tabs.onUpdated.addListener(function callback(activeInfo, info) {
-	// console.log("onActivated");
+	console.log("onUpdated");
 	// Gets tab active in current window
 	chrome.tabs.getSelected(null, function(tab){
 		if (!markedAsRead(tab.url)) {
@@ -77,7 +77,7 @@ chrome.tabs.onUpdated.addListener(function callback(activeInfo, info) {
  * 
  * Reference: https://developer.chrome.com/docs/extensions/reference/storage/
  */
-function fetchRemoteDictionary() {	
+function fetchMarkData() {	
 	chrome.storage.sync.get("visited", function (obj) {
 		if (obj["visited"] == undefined) {
 			visited = {version: 2};
@@ -87,7 +87,8 @@ function fetchRemoteDictionary() {
 				visited = objVisited;
 			} else {
 				visited = {version: 2};
-				Object.keys(objVisited).forEach(url => addUrl(url));
+				Object.keys(objVisited).forEach(
+					url => addUrl(url)					);
 			}
 		}
 	});
@@ -112,7 +113,7 @@ function updateRemoteDictionary() {
  * @param {*} atabId 
  */
 function markAsNotVisited(atabId) {
-	// console.log("markAsNotVisited");
+	console.log("markAsNotVisited");
 	chrome.browserAction.setIcon({path: "notvisited.png", tabId: atabId});
 	updateRemoteDictionary();
 }
@@ -123,7 +124,7 @@ function markAsNotVisited(atabId) {
  * @param {*} atabId 
  */
 function markAsVisited(atabId) {
-	// console.log("markAsVisited");
+	console.log("markAsVisited");
 	chrome.browserAction.setIcon({path: "visited.png", tabId: atabId });
 	updateRemoteDictionary();
 }
@@ -173,9 +174,13 @@ chrome.runtime.onMessage.addListener(function (msg) {
  * @param {*} url 
  */
 function removeUrl(url) {
+	console.log("Remove URL")
 	var key = getKey(url);
+	console.log(`Key ${key}`)
 	var path = url.replace(key, '');
+	console.log(`Path ${path}`)
 	const index = visited[key].indexOf(path);
+	console.log(`Index ${index}`)
 	if (index > -1) {
 		visited[key].splice(index, 1);
 	}
@@ -206,8 +211,11 @@ function markedAsRead(url) {
  * @param {*} url 
  */
 function addUrl(url){
+	console.log("Add URL")
 	var key = getKey(url);
+	console.log(`Key ${key}`)
 	var path = url.replace(key, '');
+	console.log(`Path ${path}`)
 	if(visited[key]) {
 		visited[key].push(path);
 	} else {
